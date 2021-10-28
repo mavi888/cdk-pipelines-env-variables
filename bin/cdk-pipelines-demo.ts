8b563f9c35abf9de6dbc812bcbb334b523f78740
Villalba <lmv@a483e7ad5050.ant.amazon.com>
2021-10-28 12:42:06 +0300

    adding developer env

--- bin/cdk-pipelines-demo.ts
+++ bin/cdk-pipelines-demo.ts
@@ -2,6 +2,7 @@
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { CdkpipelinesDemoPipelineStack} from '../lib/cdkpipelines-demo-pipeline-stack';
+import { CdkpipelinesDemoStage } from '../lib/cdkpipelines-demo-stage';
 
 import * as config from '../config.json'  
 
@@ -9,5 +10,9 @@ const app = new cdk.App();
 new CdkpipelinesDemoPipelineStack(app, 'CdkpipelinesDemoPipelineStack', {
   env: { account: config.cdk_pipeline.build_environment.account, region: config.cdk_pipeline.build_environment.region },
 });
+
+new CdkpipelinesDemoStage(app, 'Developer-Personal', {
+  env: { account: config.environments.personal.env.account, region: config.environments.personal.env.region },
+})
 
 app.synth();
